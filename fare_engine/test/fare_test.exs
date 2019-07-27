95cac90f00efd780ae657c95232a7a90ae439ee1
Sandesh Soni <hello@sandeshsoni.com>
2019-07-28 00:18:47 +0530

    WIP update test, add iex commands

---
+++ fare_engine/test/fare_test.exs
@@ -0,0 +1,20 @@
+defmodule FareEngine.FareTest do
+  use ExUnit.Case
+
+  alias FareEngine.FareWithSurcharge
+  alias FareEngine.SurchargeCalculator
+
+  test "foo bar" do
+    # create s_pid
+    # send to, from or demand and supply
+    fws_pid = spawn(FareWithSurcharge, :fare, [])
+    send(fws_pid, {12, 15})
+    # Fare after surcharge of 10% will be 1000 only
+    # {12, 15}
+    # printed in iex shell
+
+    # assert_receive {:success}
+    # use self() pid to get message back to self as assert
+  end
+
+end
